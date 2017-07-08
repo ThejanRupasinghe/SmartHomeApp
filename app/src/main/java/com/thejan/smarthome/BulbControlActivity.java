4c6c5f186db8252bd58dae65398429fa2f40110e
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2017-07-08 22:48:52 +0530

    completed bulb control activity, bulb ip hardcoded

--- app/src/main/java/com/thejan/smarthome/BulbControlActivity.java
+++ app/src/main/java/com/thejan/smarthome/BulbControlActivity.java
@@ -1,18 +1,78 @@
 package com.thejan.smarthome;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class BulbControlActivity extends AppCompatActivity {
 
+    private static ToggleButton toggleButtonBulb;
+    private static TextView textViewStatus;
+
     RequestQueue queue;
     MyRequest myRequest;
+
+    // STATIC IP OF THE BULB
+    private String bulbIP = "192.168.43.76";
+
+    private static String url_base;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulb_control);
+
+        queue= Volley.newRequestQueue(this);
+        myRequest=new MyRequest();
+
+        toggleButtonListener();
+    }
+
+    public void toggleButtonListener(){
+
+        toggleButtonBulb = (ToggleButton) findViewById(R.id.toggleButton_bulb);
+        textViewStatus = (TextView) findViewById(R.id.textView_bulbStatus);
+
+        url_base = "http://" + bulbIP;
+
+        toggleButtonBulb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+
+                    String url = url_base + "/BULB=ON";
+
+                    if(myRequest.HttpRequest(queue, url) == null){
+                        Toast.makeText(BulbControlActivity.this, "Bulb On", Toast.LENGTH_SHORT).show();
+                        textViewStatus.setTextColor(Color.parseColor("#ff99cc00"));
+                        textViewStatus.setText("Bulb ON");
+                    } else {
+                        Toast.makeText(BulbControlActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+
+                    String url = url_base + "/BULB=OFF";
+
+                    if(myRequest.HttpRequest(queue, url) == null){
+                        Toast.makeText(BulbControlActivity.this, "Bulb Off", Toast.LENGTH_SHORT).show();
+                        textViewStatus.setTextColor(Color.parseColor("#ffff4444"));
+                        textViewStatus.setText("Bulb OFF");
+                    } else {
+                        Toast.makeText(BulbControlActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+        });
     }
 }
